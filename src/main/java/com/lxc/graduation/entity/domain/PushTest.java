ad83f1fbf0b49f76e7fb26f62c647cb61464bd99
839567748@qq.com <839567748@qq.com>
2021-01-29 16:12:01 +0800

    test push

---
+++ src/main/java/com/lxc/graduation/entity/domain/PushTest.java
@@ -0,0 +1,18 @@
+package com.lxc.graduation.entity.domain;
+
+/**
+ * @Author: ck
+ * @Description:
+ * @Date: Create in 16:07 2021/1/29
+ */
+public class PushTest {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
